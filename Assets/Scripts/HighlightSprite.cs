c7359939dd37331faf521234354185ab437d98a4
David Read <d.j.read-09@student.lboro.ac.uk>
2014-02-08 21:18:19 +0000

    highlight script

---
+++ Assets/Scripts/HighlightSprite.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightSprite : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+	private Color startColor;
+
+	// Use this for initialization
+	void Start ()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		startColor = spriteRenderer.color;
+	}
+
+	void OnMouseEnter()
+	{
+		spriteRenderer.color = Color.yellow;
+	}
+
+	void OnMouseExit()
+	{
+		spriteRenderer.color = startColor;
+	}
+}
