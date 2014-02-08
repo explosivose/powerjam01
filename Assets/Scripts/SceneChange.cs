0beb9acdf1659c5ffff25a5ae232659d197ff7bc
Shameen <shameen.azeez@googlemail.com>
2014-02-08 19:26:33 +0000

    scenechange bitches

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+	public string destinationScene = "";
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "player")
+			//todo: play animation?
+			Application.LoadLevel(destinationScene);
+	}
+}
