7c057e50fc25b18c4c2969956f344c7d3fba5767
Shameen <shameen.azeez@googlemail.com>
2014-02-08 20:27:49 +0000

    scenechange changes levels properly now

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class SceneChange : MonoBehaviour {
 
+	public string collidesWithTag = "";
 	public string destinationScene = "";
+
 	// Use this for initialization
 	void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == "player")
+		if (coll.gameObject.tag == collidesWithTag) {
 			//todo: play animation?
 			Application.LoadLevel(destinationScene);
+		}
 	}
 }
