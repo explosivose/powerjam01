b970db859063615e65d2192f810e64a88c534743
Daniel Cohen <analoguecolour@gmail.com>
2014-02-08 19:21:34 +0000

    added initial movement script, deleted cow stuff

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class Dog : MonoBehaviour 
 {
 	private Vector2 targetPosition = Vector2.zero;
-	
+
+	public float moveForce = 10f;			// added force to move the dog
+	public float maxSpeed = 0.5f;				// max dog speed
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -12,15 +15,30 @@ public class Dog : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
 		// Take keyboard input and work out a target position
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
 		
-		Vector3 currentPosition3 = Camera.main.WorldToScreenPoint(transform.position);
-		Vector2 currentPosition2;
-		currentPosition2 = currentPosition3.x;
-		currentPosition2 = currentPosition3.y;
+//		Vector3 currentPosition3 = Camera.main.WorldToScreenPoint(transform.position);
+//		Vector2 currentPosition2;
+//		currentPosition2 = currentPosition3.x;
+//		currentPosition2 = currentPosition3.y;
+
+		//left-right rotation of dog
+		if (Input.GetAxis("Horizontal") < 0)
+			rigidbody2D.transform.localRotation = Quaternion.Euler(0,180, 0);
+		else if (Input.GetAxis("Horizontal") > 0)
+			rigidbody2D.transform.localRotation = Quaternion.Euler(0, 0, 0);
+
+		//dog changing direction (and hasnt hit max speed)
+		if(x * rigidbody2D.velocity.x < maxSpeed)
+			rigidbody2D.AddForce(Vector2.right * x * moveForce);
+
+		// if dog velocity is greater than his max speed then set dog velocity to maxspeed
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
 	}
 }
