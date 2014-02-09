c54699ffef58be50fc0c11a9abca79c6d48615a5
mattblickem <explosivose@gmail.com>
2014-02-09 11:54:35 +0000

    updates to scene changer prefab and script...

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -3,21 +3,11 @@ using System.Collections;
 
 public class SceneChange : MonoBehaviour {
 
-	public string collidesWithTag = "";
 	public string destinationScene = "";
-
-	// Use this for initialization
-	void Start () {
-
-	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (coll.gameObject.tag == collidesWithTag) {
+		if (coll.gameObject.tag == "Player") {
 			//todo: play animation?
 			Application.LoadLevel(destinationScene);
 		}
