9101ce2a2e8be7c5e624857142d11433cf0ca94e
mattblickem <explosivose@gmail.com>
2014-02-09 12:11:54 +0000

    added dummy spawn pos thing for you guys to fill in

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SceneChange : MonoBehaviour {
 
 	public string destinationScene = "";
-	
+	public Vector2 spawnPosition;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Player") {
